47feeecf374ed2502aad1ed76e8b12237ac31a7e
HouseDeepTry <noyeye159@gmail.com>
2019-01-14 17:44:08 +0700

    Lần 1

---
+++ 2D UFO/Assets/Script/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+
+    private Rigidbody2D Rbd;
+
+    private void Start()
+    {
+        Rbd = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(MoveHorizontal,MoveVertical);
+        Rbd.AddForce(movement*Speed);
+    }
+}
